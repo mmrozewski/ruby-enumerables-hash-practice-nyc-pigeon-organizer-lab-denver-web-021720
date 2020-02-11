b40ba092614ca25a5db180f93f420ca3bec072aa
Mateusz Mrozewski <mmrozews@gmail.com>
2020-02-11 02:28:45 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,35 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  
+  all_pigeons = {}
+  
+  data.each do |descriptors, specifics|
+    specifics.each do |specifics, arr|
+      name.each do |name|
+        if !all_pigeons[name]
+            all_pigeons[name] = {}
+            all_pigeons[name][descriptors] = []
+          else
+            all_pigeons[name][descriptors] = []
+          end 
+      end
+    end
+  end
+  all_pigeons.each do |name, specifics|
+    specifics.each do |hashval, arr|
+      data.each do |descriptors, specifics|
+        specifics.each do |val, arr|
+          
+          array.each do |elem|
+            if element == name && hashval == descriptors
+              all_pigeons[name][hashval] << val.to_s
+            end
+          end
+        end
+      end
+    end
+    all_pigeons
+  end 
 end
+
+
+nyc_pigeon_organizer(pigeon_data)
