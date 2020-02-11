61a801fd6e84a1ea0cbbfb98561627cb8166ac68
Mateusz Mrozewski <mmrozews@gmail.com>
2020-02-11 02:36:26 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,12 +2,16 @@ def nyc_pigeon_organizer (data)
   final = {}
 
   data.each do |first_level, all_other|
+    
     all_other.each do |category, array|
+      
       array.each do |name|
+        
         final[name] = {:color => [], :gender => [], :lives => []}
       end 
     end 
   end 
+  
   x = final.keys
   data[:color].each do |bird_color, name|
     name.each do |bird_name|
